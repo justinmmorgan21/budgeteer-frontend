f490379ad19b641ee7f75890b6bead0094af17e3
justinmmorgan21 <justinmorgan21@gmail.com>
2025-04-12 12:51:11 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { TransactionsPage } from "./TransactionsPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <TransactionsPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
