33293fea1da171f65ffad3d7352f8f6ad659ce4c
justinmmorgan21 <justinmorgan21@gmail.com>
2025-04-17 22:24:52 -0500

    added test for category_tag post and get

--- src/components/TransactionsIndex.jsx
+++ src/components/TransactionsIndex.jsx
@@ -1,8 +1,10 @@
+import { useEffect } from "react";
+import axios from 'axios';
+
 export function TransactionsIndex({transactions, categories}) {
   const handleSelect = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
-    console.log(params);
     console.log(params.get("categories"));
   }
 
@@ -15,13 +17,13 @@ export function TransactionsIndex({transactions, categories}) {
   }
 
   const TransactionItem = ({ t }) => (
-    <div style={{border:"solid black 1px", padding:"6px"}}>
+    <div style={{border:"solid black 1px", padding:"10px"}}>
       <span style={{display: "inline-block", width:"140px"}}>{t.type}</span>
       <span style={{display: "inline-block", width:"150px"}}> {formatDate(t.date)}</span>
       <span style={{display: "inline-block", width:"100px"}}> ${t.amount}</span>
       <span style={{display: "inline-block", width:"600px"}}> {t.payee}</span>
       <form onSubmit={handleSelect} action="" style={{  width:"fit-content", display:"inline"}}>
-        <label htmlFor="categories">Category: </label>
+        <label htmlFor="categories"></label>
         <select name="categories" >
           {categories.map(category => (
             <option key={category.id} value={category.name}>{category.name}</option>
@@ -33,8 +35,26 @@ export function TransactionsIndex({transactions, categories}) {
     </div>
   )
 
+  const doThis = () => {
+    console.log("post");
+    const params = new FormData();
+    params.append("category", 99);
+    params.append("tag", 99);
+    axios.post("http://localhost:5000/category_tags", params).then(response => {
+      console.log(response.data);
+    })
+  }
+  const doThat = () => {
+    console.log("get");
+    axios.get("http://localhost:5000/category_tags").then(response => {
+      console.log(response.data);
+    })
+  }
+
   return (
     <div>
+      <button onClick={()=>doThis()}>POST</button>
+      <button onClick={()=>doThat()}>GET</button>
       <h1>All transactions</h1>
       <div style={{ padding:"6px"}}>
         <span style={{display: "inline-block", width:"140px"}}>type</span>
--- src/components/TransactionsPage.jsx
+++ src/components/TransactionsPage.jsx
@@ -3,7 +3,7 @@ import { useLoaderData } from "react-router-dom";
 
 export function TransactionsPage() {
   const { transactions, categories } = useLoaderData();
-
+  
   return (
     <main>
       <TransactionsIndex transactions={transactions} categories={categories}/>
