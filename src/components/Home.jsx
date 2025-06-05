0d1b74bca15b2864c338019c3f893ad451f2c103
justinmmorgan21 <justinmorgan21@gmail.com>
2025-06-05 11:06:53 -0500

    added default apexcharts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,25 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+// export function Home() {
+//   return (
+//     <div style={{width:"80%", margin:"20px auto"}}>
+//       <h1>-- Dashboard goes here --</h1>
+//       <Link to="/transactions">Go to transactions</Link>
+//     </div>
+//   )
+// }
+
+
+import { BarChart } from "./BarChart"; // adjust path if needed
+import { LineChart } from "./LineChart";
+
 export function Home() {
   return (
-    <div style={{width:"80%", margin:"20px auto"}}>
-      <h1>-- Dashboard goes here --</h1>
-      <Link to="/transactions">Go to transactions</Link>
+    <div>
+      <h1>Dashboard</h1>
+      <div style={{display:"grid", gridTemplateColumns:"auto auto"}}>
+        <BarChart />
+        <LineChart />
+      </div>
     </div>
-  )
-}
+  );
+};
