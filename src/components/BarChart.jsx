80ed3121167fe78c1e6e4c9be5c404351824dac3
justinmmorgan21 <justinmorgan21@gmail.com>
2025-06-07 10:40:27 -0500

    split dashboard into small and big budgets

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+// import React from "react";
 import ReactApexChart from "react-apexcharts";
+import React, { useState } from "react"
 
-export function BarChart(data) {
-  const categories = data.data;
+export function BarChart({data, title}) {
+  const [categories, setCategories] = useState(data);
   console.log(categories[0]);
   const [state] = React.useState({
+    title: title,
     series: [
       {
         name: "Budget",
         // data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-        data: categories.map(cat => cat.name == "Income" ? null : cat.budget_amount)
+        data: categories.filter(cat => cat.name != "Income").map(cat => cat.budget_amount)
       },
       {
         name: "Actual",
         // data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-        data: categories.map(cat => cat.name == "Income" ? null : cat.accumulated)
+        data: categories.filter(cat => cat.name != "Income").map(cat => cat.accumulated)
       }
     ],
     options: {
@@ -40,7 +42,7 @@ export function BarChart(data) {
       },
       xaxis: {
         // categories: ["Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
-        categories: categories.map(cat => cat.name == "income" ? null : cat.name)
+        categories: categories.filter(cat => cat.name != "Income").map(cat => cat.name)
       },
       yaxis: {
         title: {
@@ -60,12 +62,14 @@ export function BarChart(data) {
 
   return (
     <div id="chart">
+      <h2>{state.title}</h2>
       <ReactApexChart
         options={state.options}
         series={state.series}
         type="bar"
         height={550}
       />
+      {/* <button onClick={()=>console.log(title)}>PRINT LOG 2</button> */}
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,19 +11,22 @@
 import { useLoaderData } from "react-router-dom";
 import { BarChart } from "./BarChart"; // adjust path if needed
 import { LineChart } from "./LineChart";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function Home() {
   const loadedCategories = useLoaderData();
-  const [ categories, setCategories ] = useState(loadedCategories);
-  // console.log(categories);
+  const [ bigCategories, setBigCategories ] = useState(loadedCategories.filter(cat => cat.budget_amount >= 200));
+  const [ smallCategories, setSmallCategories ] = useState(loadedCategories.filter(cat => cat.budget_amount < 200));
+
   return (
     <div>
       <h1>Dashboard</h1>
-      {/* <div style={{display:"grid", gridTemplateColumns:"auto auto"}}> */}
-        <BarChart data={categories}/>
-      {/* </div> */}
+      <div style={{display:"grid", gridTemplateColumns:"auto auto"}}>
+        <BarChart data={smallCategories} title={"Small Budgets"}/>
+        <BarChart data={bigCategories} title={"Big Budgets"}/>
+      </div>
         {/* <LineChart /> */}
+      {/* <button onClick={()=>console.log(loadedCategories)}>PRINT LOG 1</button> */}
     </div>
   );
 };
